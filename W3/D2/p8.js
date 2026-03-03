ab646e3e4a3c06ae516c5dcaad6b48cc082b0a64
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-03-03 20:13:59 +0530

    default parameters

---
+++ W3/D2/p8.js
@@ -0,0 +1,13 @@
+// default parameters
+function product (a,b=1){
+    return a*b;
+}
+console.log("product of 15 and 4",product(15,4));
+console.log("product of 15 ",product(15));
+
+//Rest parameters
+function sumOfAll(...numbers){
+    console.log(...numbers);
+}
+sumOfAll(1,2,3);
+sumOfAll(10);
