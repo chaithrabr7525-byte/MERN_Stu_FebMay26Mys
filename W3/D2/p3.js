81120c065d4d3cde96296b9b9357643c7af8d12a
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-03-03 20:10:29 +0530

    logical operators

---
+++ W3/D2/p3.js
@@ -0,0 +1,6 @@
+//logical operators
+let a = true, b = false;
+console.log("logical operators");
+console.log("a&&b =", a && b);//logical AND
+console.log("a||b =", a || b);//logical OR
+console.log("!a =", !a);//logical NOT
