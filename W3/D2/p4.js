9522b59f5c3de7f782ae4769056fb5d5267f6ad4
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-03-03 20:11:15 +0530

    conditional statement

---
+++ W3/D2/p4.js
@@ -0,0 +1,33 @@
+// //conditional statements
+// let age = 20;
+// if(age < 13){
+  
+//     console.log("child");
+// }
+// else if(age<18){
+//     console.log("teenaager");
+// }
+// else
+// {
+//     console.log("adult");
+// }
+ //switch statements
+ console.log("conditional statements")
+ let day = "tuesday";
+ switch (day){
+    case "monday":
+    console.log("start of the week");
+    break;
+    case "wednesday":
+        console.log("mid of the week");
+        break;
+        case "friday":
+            console.log("end of work week");
+            default:
+                console.log("some day in the week");
+                break;
+
+ }
+ //try conversion 
+ let n = Number("ABC");
+ console.log("n:",n,"type of n:", typeof n,"isNAN","isNAN"(n));
