a32108971c781840061f514dcde8c64544219a7e
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-03-04 14:11:36 +0530

    basics of java

--- W3/D3/p5.js
+++ W3/D3/p5.js
@@ -7,5 +7,26 @@ const person ={name:"chaithra",
 console.log(person.name);//dot notation
 console.log(person.age);
 console.log(person.city);
+
 //bracket notation
 console.log("name",person.name);
+
+//add a new property
+person.id="12345";
+console.log("person",person);
+
+//modify
+person.age =20;
+console.log("person",person)
+
+//delete
+delete person.id;
+console.log("person",person)
+
+//object constructor
+const car= new Object();
+car.make ="BMW";
+car.model ="A4";
+car.year ="2k26";
+console.log("car",car)
+
