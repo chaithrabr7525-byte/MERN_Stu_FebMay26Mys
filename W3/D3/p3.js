7307ccc4a431bd623a95f8a96935cc85aa0c6063
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-03-04 12:40:32 +0530

    break & continue

---
+++ W3/D3/p3.js
@@ -0,0 +1,19 @@
+ for (let i=0; i<5; i++){//forward loop
+if (i === 2) {
+    console.log("i", i);
+    break;
+}
+ }
+
+// continue
+for (let i = 0; i < 5; i++) {//forward loop
+    if (i === 2) {
+        console.log("i", i);
+        continue;
+    }
+}
+
+
+
+
+
