699163916d0e8724cfab9326c9b17ab41ea1a618
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-03-04 12:43:50 +0530

    basics of java

---
+++ W3/D3/p5.js
@@ -0,0 +1,11 @@
+//basics of java
+const person ={name:"chaithra",
+    age:21,
+    city:"krpete"};
+
+//console.log("person",person);
+console.log(person.name);//dot notation
+console.log(person.age);
+console.log(person.city);
+//bracket notation
+console.log("name",person.name);
