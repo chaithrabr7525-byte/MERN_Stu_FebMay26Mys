cff95ee4c47620c358d00307388f914e8bb638ad
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-03-15 18:56:05 +0530

    calculateToyal

---
+++ W4/D1/p5.js
@@ -0,0 +1,22 @@
+// BreakPoint
+//Inspects variable values
+// View the call stack
+// Step through code line by line
+// Evaluate expressions in the console
+// watch how variables change during the execution
+// To find logical errors
+
+function calculateTotal(prices){
+    let total = 0;
+    for(let i = 0;i<prices.length;i++){
+        let price = prices[i];
+        debugger;
+        total+=price;
+    }
+    return total;
+}
+
+let cart = [100,250,150,1000,220];
+console.log("total: ",calculateTotal(cart));
+
+console.log(document.body);
