12e5a5b293b012c9f8dded9d2d1ce104bd4ed1a4
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-03-25 10:43:04 +0530

    Reading & writing files sync

--- W6/D3/p2.js
+++ W6/D3/p2.js
@@ -7,9 +7,18 @@ const path =  require("path");
 //moduleName.functionName()
 const filepath = path.join(__dirname,"sync-note.txt");
 
-fs.writeFileSync(filepath,"This file was written using writeFileSync().\nSynchronous operation block execution");
+//fs.writeFileSync(filepath,"This file was written using writeFileSync().\nSynchronous operation block execution");
 //append to a file
+fs.appendFile(filepath,"New text added using fs.appendFile",
+function(error){
+    if(error){
+        console.log("Append error:",error.message);
+        return;
+    }
+    console.log("File content appended");
+}
+);
 console.log("File written synchronously.");
 const content = fs.readFileSync(filepath,"utf-8");
 console.log("File read synchronously");
-console.log(content);
+console.log("File content:\n", content);
