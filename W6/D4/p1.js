02a0b45d98b8384e233faa31e901e499411c3cd9
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-03-26 16:03:38 +0530

    understanding the HTTP module

---
+++ W6/D4/p1.js
@@ -0,0 +1,7 @@
+//understanding the HTTP module
+
+//importing built-in http module 
+const http = require("http");
+
+console.log("Type of http.createServer:",typeof http.createServer);
+console.log("common HTTP methods:",http.METHODS);
