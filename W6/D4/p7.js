025b1abb9cd28cf7d5d50cb8a99a4ba24cc5cfc7
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-03-26 17:25:58 +0530

    Parsing query parameters

---
+++ W6/D4/p7.js
@@ -0,0 +1,24 @@
+//Parsing query parameters
+const http = require ("http");
+const {URL} = require ("url");
+
+const server = http.createServer(function(req,res){
+const fullurl = new URL(req.url,"http://localhost:3001");
+if(req.method == "GET" && fullurl.pathname ==="/search"){
+    const term =fullurl.searchParams.get("term");
+     const page =fullurl.searchParams.get("page");
+
+     res.writeHead(200,{"content-type":"application/json"});
+     res.end(JSON.stringify({
+        route:"/search",
+        term:term,
+        page:page
+     }));
+    return;
+}
+res.writeHead(404,{"content-type":"application/json"});
+    res.end(JSON.stringify({success:false,Message:"Route not found"}));
+});
+server.listen(3001,function(){
+    console.log("Server is running at http://localhost:3001");
+});
