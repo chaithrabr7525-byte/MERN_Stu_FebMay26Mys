60bdecf348ad0905ab10c555f479ab5418c35404
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-03-30 22:54:13 +0530

    portfolio features

---
+++ assignment/portfolio project v3/js/data/education.js
@@ -0,0 +1,23 @@
+const educationData = [
+  {
+    institution: "Cauvery Institute Of Technology Mandya",
+    degree: "Bachelor of Engineering in Computer Science",
+    duration: "2023 - 2026",
+    location: "Mandya, Karnataka",
+    score: "CGPA: 8.1"
+  },
+  {
+    institution: "Shree Bahubali Politechnic",
+    degree: "Diploma",
+    duration: "2020 - 2023",
+    location: "Shravana Belagula, Karnataka",
+    score: "CGPA: 9.0"
+  },
+  {
+    institution: "Cambridge Public School",
+    degree: "High School",
+    duration: "2011 - 2020",
+    location: "K R PETE, Karnataka",
+    score: "Percentage: 77%"
+  }
+];
