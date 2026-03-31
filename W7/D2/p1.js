34b37276038e49b632e33d53a43f21abb6c07899
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-03-31 16:50:23 +0530

    Routing'

--- W7/D2/p1.js
+++ W7/D2/p1.js
@@ -9,22 +9,44 @@ const app = express();
 //Router objects helps organize route groups
 const apiRouter = express.Router();
 
-apiRouter.get("/users",function(req,res){
+apiRouter.get("/users", function (req, res) {
     res.json({
-        route:"/api/users",
+        route: "/api/users",
         message: "Users route inside api router"
     });
 });
 
-apiRouter.get("/orders",function(req,res){
+apiRouter.get("/orders", function (req, res) {
     res.json({
-        route:"/api/users",
+        route: "/api/users",
         message: "Users route inside api router"
     });
 });
 //Mounting the router under the /api base path
-app.use("/api",apiRouter);
+app.use("/api", apiRouter);
+//***new Router***
+//Product router (handle/api/products/...)
+const productsRouter = express.Router();
+//api/products           ->create product
+productsRouter.post("/", (req, res) => {
+    res.json({
+        route: "/api/product",
+        message: "Create products",
+    });
+});
 
-app.listen(4000,function(){
+//api/products/:id       -> delete product
+productsRouter.delete("/:id", (req, res) => {
+    res.json({
+        route: `/api/products/${req.params.id}`,
+        message: "Delete product"
+
+    });
+});
+
+//mount products router under /api/products
+apiRouter.use("/products", productsRouter);
+
+app.listen(4000, function () {
     console.log("Express server running at http://localhost:4000");
 });
