5fb936b584b3157232693ffcc0fe576b084e383f
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-08 10:07:05 +0530

    Storing User's data application

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/users.js
@@ -0,0 +1,18 @@
+//Storing User's data application
+const users = [
+    {
+        id:1,
+        name:"Bubu Rani",
+        email:"u@e.com",
+        password:"Buri123",
+        role:"user"
+    },
+    {
+        id:2,
+        name:"Dudu King",
+        email:"a@e.com",
+        password:"admin123",
+        role:"admin"
+    },
+];
+module.exports = users;
