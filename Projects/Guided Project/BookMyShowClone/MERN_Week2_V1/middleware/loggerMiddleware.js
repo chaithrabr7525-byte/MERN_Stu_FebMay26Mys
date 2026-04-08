07e66aaefab9a12fa7cb5a746f86718203f161e6
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-08 10:30:43 +0530

    Middleware for request log

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/loggerMiddleware.js
@@ -0,0 +1,8 @@
+//Middleware for request log
+function loggerMiddleware(req,res,next){
+    console.log(`${req.method}${req.originalUrl}`);
+    next();
+
+}
+module.exports = loggerMiddleware;
+//Task: write the log to a file along with timestamp with both req & res
