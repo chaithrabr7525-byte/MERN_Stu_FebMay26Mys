209796e96a3b1e547fd0da5c81121c8671463c0d
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-09 11:37:18 +0530

    Main express server entry points

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/server.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/server.js
@@ -1,7 +1,16 @@
-//Main express server entry point
+//Main express server entry points
 const app = require ("./app");
 
 const PORT = 5000;
 app.listen(PORT,()=>{
     console.log(`sever running on http://localhost:${PORT}`);
 });
+
+//http://localhost:5000/auth/login
+//req->server.js --->app.js-->authRoutes-->authController-->loginUser()
+//loginUser()
+//1. success:token->cookie->session response:200
+//2. Failure:error->
+// CustomErrror -->
+//1. email/password are required.,400
+//2. invalid email/password.,401
