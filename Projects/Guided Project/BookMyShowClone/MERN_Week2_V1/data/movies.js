a78c28f5275ec3e1805bf3bc42f0eec5a8aadc6d
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-11 13:17:23 +0530

    movies.jst this file stores the movies data used in the CLI App

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/movies.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/movies.js
@@ -1,5 +1,5 @@
 // movies.js
-// This file stores the movies data used in the CLI App
+// This file stores the movie data used in the CLI App
 
 const movies = [
     {
@@ -7,41 +7,40 @@ const movies = [
         title:"Dhurandar2",
         language:"Hindi",
         genre:"Action Thriller",
-        city:"Mysuru",
+        city: "Mysuru",
         cinema:"Nexus Mall-Screen 1",
         showtimes:[
-            {time:"10:00 AM",seatsAvailable: 250},
-            {time:"1:00 PM",seatsAvailable: 250},
-            {time:"6:00 PM",seatsAvailable: 250}
+            {time: "10:00 AM",seatsAvailable: 250},
+            {time: "1:00 PM",seatsAvailable: 250},
+            {time: "6:00 PM",seatsAvailable: 250}
         ]
     },
     {
         id:2,
         title:"LoveMocktail3",
-        language:"Hindi",
+        language:"Kannada",
         genre:"Romantic",
-        city:"Mysuru",
+        city: "Mysuru",
         cinema:"DRC Mall-Screen 2",
         showtimes:[
-            {time:"10:00 AM",seatsAvailable: 100},
-            {time:"1:00 PM",seatsAvailable: 70},
-            {time:"6:00 PM",seatsAvailable: 200}
+            {time: "10:00 AM",seatsAvailable: 100},
+            {time: "1:00 PM",seatsAvailable: 70},
+            {time: "6:00 PM",seatsAvailable: 200}
         ]
     },
     {
         id:3,
-        title:"Kantara",
+        title:"Hayagreeva",
         language:"Kannada",
         genre:"Action",
-        city:"mysuru",
-        cinema:"BMH Mall-Screen 2",
+        city: "Davangere",
+        cinema:"Davangere Mall-Screen 1",
         showtimes:[
-            {time:"11:00 AM",seatsAvailable: 100},
-            {time:"2:00 PM",seatsAvailable: 70},
-            {time:"7:00 PM",seatsAvailable: 150}
+            {time: "11:00 AM",seatsAvailable: 100},
+            {time: "2:00 PM",seatsAvailable: 70},
+            {time: "7:00 PM",seatsAvailable: 200}
         ]
     }
 ];
-
-// Export the movie data so that other files can use it.
+//Export the movie data so that other files can use it. 
 module.exports = movies;
