005c5211b7c6fd300fc980e55907a2fa8f36c4a0
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-11 13:23:01 +0530

    Handling requests related to bookings

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/routes/bookingRoutes.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/routes/bookingRoutes.js
@@ -1,12 +1,24 @@
-//handling req related to bookings
+// Handling requests related to bookings
 const express = require("express");
 const { authMiddleware } = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
-const  {bookingValidationHandler,createBooking,getAllBookings,getMyBookings}=require("../controllers/bookingController")
+const {
+    bookingValidationHandler,
+    createBooking,
+    getAllBookings,
+    getMyBookings
+} = require("../controllers/bookingController");
+
 const router = express.Router();
 
-router.post("/bookings",authMiddleware,bookingValidationHandler,createBooking);//Create a booking /to book tickets
-router.get("/bookings/me",authMiddleware,getMyBookings);
-router.get("/bookings",authMiddleware,getAllBookings,("admin"),getAllBookings);
-module.exports = router;
+//Create a booking / to book ticket/s
+router.post("/bookings",authMiddleware,
+    bookingValidationHandler,
+    createBooking);
 
+router.get("/bookings/me",authMiddleware,getMyBookings);
+router.get("/bookings",
+    authMiddleware,
+    roleMiddleware("admin"),
+    getAllBookings);
+module.exports = router;
