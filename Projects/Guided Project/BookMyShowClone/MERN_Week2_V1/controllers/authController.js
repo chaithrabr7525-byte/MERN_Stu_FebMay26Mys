3dff1463914bda537f5dd022150de98773c8d31d
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-11 13:15:17 +0530

    Created User login, profile fetch & logout of user functionality

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/controllers/authController.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/controllers/authController.js
@@ -1,62 +1,68 @@
-//Created User login, profile fetch & logout of user functionality 
+// User Login, profile fetch and logout of user functionality created
 const jwt = require("jsonwebtoken");
-const user = require(".../data/users");
+const users = require("../data/users");
 
-const CustomError = require("../util/CustomError");
+const CustomError = require("../utils/CustomError");
 
-const { JWT_SECRET } = require(".../middleware/authMiddleware");
+const {JWT_SECRET} = require("../middleware/authMiddleware");
 
-function loginUser(req, res, next) {
-    try {
-        const { email, password } = req.body;
+function loginUser(req,res,next){
+    try{
+        const {email,password} = req.body;
+
         if (!email || !password) {
-            return next(new CustomError("email/password are required.", 401));
+            return next(new CustomError("email/password are required.",400));
         }
-        const user = useSyncExternalStore.find((u) => u.email === email && u.password === password);
-        if (!user) {
-            return next(new CustomError("Invalid email/password", 401));
-        }
-        const token = jwt.sign({
-            id: user.id,
-            name: "user.name",
-            email: "user.email",
-            role: "user.role"
-        }, JWT_SECRET, { expiresIn: "30m" });
+        const user = users.find((u)=>u.email === email && u.password === password);
 
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: false,
-            maxAge: 60 * 60 * 1000
+        if(!user){
+           return next(new CustomError("Invalid email/password",401));
+        }
+
+        const token = jwt.sign({
+            id:user.id,
+            name:user.name,
+            email:user.email,
+            role:user.role
+        },JWT_SECRET,{expiresIn:"30m"});
+
+        res.cookie("token",token,{
+            httpOnly:true,
+            secure:false,
+            maxAge:60*60*1000
         });
+
         req.session.user = {
-            id: user.id,
-            name: "user.name",
-            email: "user.email",
-            role: "user.role"
+            id:user.id,
+            name:user.name,
+            email:user.email,
+            role:user.role
         };
+
         res.status(200).json({
             success:true,
-            message:"Login successfull",
+            message:"Login Successful",
             token,
             user:{
-                 id: user.id,
-            name: "user.name",
-            email: "user.email",
-            role: "user.role"
+                id:user.id,
+                name:user.name,
+                email:user.email,
+                role:user.role
             }
         });
-    }   
+    }
     catch(error){
         next(error);
     }
 }
+
 function logoutUser(req,res,next){
     try{
         req.session.destroy(()=>{
             res.clearCookie("token");
             res.status(200).json({
-                 success:true,
-            message:"Logout successfull",
+                success:true,
+                message:"Logout Successful"
             });
         });
     }
@@ -64,19 +70,21 @@ function logoutUser(req,res,next){
         next(error);
     }
 }
+
 function getProfile(req,res,next){
     try{
         res.status(200).json({
             success:true,
-            message:"Profile fetched successfully",
+            message:"Profile fetched Successfully",
             user:req.user,
-            sessionUser:req.session.user || null
+            sessionUser: req.session.user || null
         });
     }
     catch(error){
         next(error);
     }
 }
+
 module.exports = {
     loginUser,
     logoutUser,
