104b95b5f17f19a458b3d32e7c50505b50ddc25b
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-11 13:21:45 +0530

    roleMiddleware helps to find who can access what

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/roleMiddleware.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/roleMiddleware.js
@@ -1,24 +1,15 @@
-//roleMiddleware helps to find who can access what 
-const CustomError = require("../util/CustomError");
+//Checks the permission for the req and allows it or rejects it
+const CustomError = require("../utils/CustomError");
 
 function roleMiddleware(...allowedRoles){
     return(req,res,next)=>{
-        if(!req.user){
-return next(new CustomError("User Info Not Found ",401));
+        if (!req.user) {
+            return next(new CustomError("User info not found.",401));
         }
-        if(!allowedRoles.includes(req.user.role)){
-return next(new CustomError("Forbidden: you do not have access to this resource",401));
+        if (!allowedRoles.includes(req.user.role)) {
+            return next(new CustomError("Forbidden: You do not have access to this resource.",401));
         }
         next();
     };
 }
 module.exports = roleMiddleware;
-
-
-
-
-
-
-
-
-
