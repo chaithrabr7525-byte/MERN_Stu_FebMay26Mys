e56331c80c185c1c7fca05ef2160d4842f04b841
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-11 13:24:44 +0530

    Fetching the user booking based on userID

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/routes/userRoutes.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/routes/userRoutes.js
@@ -1,22 +1,9 @@
-//Fetching the user booking based on userID
-const express = require("express");
-const { authMiddleware } = require("../middleware/authMiddleware");
-const { bookings } = require("../controllers/bookingController");
+//Custom error class created for error handling
+class CustomError extends Error{
+    constructor(message,statusCode){
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
 
-const router = express.Router();
-router.get("/:userId/booking", authMiddleware, (req, res) => { //get booking -> To know about specific user ID
-    const userId = Number(req.params.userId);
-    const userbookings = bookings.filter((booking) =>
-        booking.userId === userId);
-    res.status(200).json({
-       success:true,
-       count: userbookings.length,
-       data: userbookings
-    });
-});
-module.exports = router;
-
-
-
-
-
+module.exports = CustomError;
