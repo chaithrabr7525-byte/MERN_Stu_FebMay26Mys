81036e853997065a01725fbf965f6edc23523b3f
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-11 13:20:18 +0530

    Generic error handler is created

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/errorMiddleware.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/errorMiddleware.js
@@ -1,10 +1,12 @@
-//Creating Generic error handler
-function errorMidlleware(err,req,res,next){
-    const statusCode = err.statusCode || 500;//during the error setup would we not set it take by default as 500
- res.status(statusCode).json({
-    success:false,
-    message:err.message || "Internal server error"
- });
+// Generic error handler is created
 
+function errorMiddleware(err,req,res,next){
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        success: false,
+        message: err.message || "Internal server error"
+    });
 }
-module.exports = errorMidlleware;
+
+module.exports = errorMiddleware;
