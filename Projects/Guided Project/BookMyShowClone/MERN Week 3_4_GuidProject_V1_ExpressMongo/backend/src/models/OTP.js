e432c846ccf9dea92b02666c9828093cae90830f
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-21 12:55:00 +0530

    created OTPSchema, addedTTL index, created OTPModel & exported

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/OTP.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const otpSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        index: true,
+    },
+    otp: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    expiresAt: {
+        type: Date,
+        required: true,
+    },
+    attempts: {
+        type: Number,
+        default: 0,
+    },
+}, { timestamps: true, });
+//TTl index
+otpSchema.index({ expiresAt: 1 }, { expiresAfterSeconds: 0 });
+//export
+module.exports = mongoose.model("OTP", otpSchema);
