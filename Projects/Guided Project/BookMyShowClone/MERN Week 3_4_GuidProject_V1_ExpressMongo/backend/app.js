83a01094005f6cb63c612bd91ad49dba792f85ea
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-21 15:00:16 +0530

    Routes related to auth is created

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,11 +1,14 @@
-
 const express = require("express");
+const authRoutes = require("./routes/auth.routes");
+
 const app = express();
 app.use(express.json());
-app.get("/",(req,res)=>{
+app.use("/api/auth",authRoutes)
+//base url
+app.get("/", (req, res) => {
     res.status(200).json({
-   success:true,
-    message: "Movie booking API is running...",
-});
+        success: true,
+        message: "Movie booking API is running...",
+    });
 });
 module.exports = app;
