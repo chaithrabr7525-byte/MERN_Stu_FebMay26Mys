3333c2d287b34004b760d003ceb3c50eb3ff3039
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-22 13:08:27 +0530

    created Role middleware for RABC

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/role.middleware.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/role.middleware.js
@@ -1,7 +1,7 @@
 // Role moddleware: RBAC
 exports.authorize = (...roles) => {
     return (req, res, next) => {
-        if (!req.user || !roles.includes(req.user.rule)) {
+        if (!req.user || !roles.includes(req.user.role)) {
             return res.status(403).json({
                 success: false,
                 message: "Access denied: insufficient permission",
