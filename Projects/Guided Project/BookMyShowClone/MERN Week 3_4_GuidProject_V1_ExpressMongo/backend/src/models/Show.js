d9c24f7543d74f5eb2fe486a87ba6b202511ee65
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-23 14:36:25 +0530

    typo

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Show.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Show.js
@@ -1,58 +1,72 @@
 const mongoose = require("mongoose");
+//sub schema
 const seatSchema = new mongoose.Schema({
     seatNumber:{
         type:String,
-        required:true,
+        required:true
     },
     isBooked:{
         type:Boolean,
-        default:false,
-    },
-},{_id:false});
-const showSchema = new mongoose.Schema({
-movieId:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Movie",
-    required:true,
-    index:true,
-},
-date:{
-    type:Date,
- required:true,
- index:true,
-},
-time:{
-    type:String,
-    required:true,
-},
-totalSeats:{
-    type:Number,
-    required:true,
-},
-availableSeats:{
-    type:Number,
-    required:true,
-},
-seats:{
-    type:[seatSchema],
-    required:true,
-},
-isActive:{
-    type:Boolean,
-    default:true,
-},
+        default:false
+    }
 },{
-    timestamps:true,
+    _id:false
 });
 
-//compound index:
+const showSchema = new mongoose.Schema({
+    movieId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Movie",
+        required:true,
+        index:true
+    },
+    date:{
+        type:Date,
+        required:true,
+        index:true
+    },
+    time:{
+        type:String,
+        required:true
+    },
+    totalSeats:{
+        type:Number,
+        required:true
+    },
+    availableSeats:{
+        type:Number,
+        required:true
+    },
+   seats:{
+    type:[seatSchema],
+    required:true
+   },
+   isActive:{
+    type:Boolean,
+    default:true
+   }
+},
+{
+    timestamps:true
+});
+
+
+//Compound index
 showSchema.index({movieId:1,date:1});
 
-//Add validation
-showSchema.pre("save",function(next){
-    if(this.availableSeats>this.totalSeats){
-        return next(new Error("Available seats cannot exceed total seats"));
+// //Add Validation
+// showSchema.pre("save",function(next){
+//     if(this.availableSeats>this.totalSeats){
+//         return next(new Error("Available seats cannot exceed total seats"));
+//     }
+//     next();
+// });
+
+// New Add Validation code
+showSchema.pre("save", async function () {
+    if (this.availableSeats > this.totalSeats) {
+        throw new Error("Available seats cannot exceed total seats");
     }
-    next();
 });
-module.exports = mongoose.model("Show",showSchema);
+
+module.exports = mongoose.model("Show",showSchema)
