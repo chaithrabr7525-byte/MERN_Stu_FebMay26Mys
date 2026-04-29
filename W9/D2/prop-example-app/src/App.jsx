23625adc38ebb38dbb9feaea86e3aa224c422161
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-29 12:12:32 +0530

    comments added

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -4,6 +4,7 @@ import { PropDestructuring } from './components/P2'
 import { PropsChildren } from './components/P3';
 import {PropDrillingDemo} from './components/P4.jsx'
 import{SharedStateParent} from './components/p5.jsx'
+import{CompositionDemo} from './components/p6.jsx'
 function App() {
   return (
     <>
@@ -11,7 +12,8 @@ function App() {
       {/* <PropDestructuring/> */}
       {/* <PropsChildren/> */}
       {/* <PropDrillingDemo/> */}
-      <SharedStateParent/>
+      {/* <SharedStateParent/> */}
+      <CompositionDemo/>
     </>
   );
 }
