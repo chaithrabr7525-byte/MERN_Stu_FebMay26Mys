6e2df78497ed009137e5fa2feb0508e04e8800e7
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-29 12:12:08 +0530

    feat: add UserProfile component with prop destructuring example

---
+++ W9/D2/prop-example-app/src/components/p2.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+//prop destructuring
+//Child Components
+function UserProfile(username,skill){
+    return(
+        <div>
+            <p>User: {username}</p>
+            <p>Skill: {skill}</p>
+        </div>
+    )
+}
+
+//Parent component
+export function PropDestructuring(){
+    return(
+        <>
+        <h2>Prop Destructuring</h2>
+        <UserProfile username="Abhi" skill="React"/>
+        </>
+    );
+}
