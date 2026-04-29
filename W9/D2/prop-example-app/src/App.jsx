9d34b3ec4fdc4c2a3aefcca9e6ad4669e428adf4
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-29 11:44:09 +0530

    state lifting

---
+++ W9/D2/prop-example-app/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { PropBasics } from './components/P1'
+import { PropDestructuring } from './components/P2'
+import { PropsChildren } from './components/P3';
+import {PropDrillingDemo} from './components/P4.jsx'
+import{SharedStateParent} from './components/p5.jsx'
+function App() {
+  return (
+    <>
+      {/* <PropBasics/> */}
+      {/* <PropDestructuring/> */}
+      {/* <PropsChildren/> */}
+      {/* <PropDrillingDemo/> */}
+      <SharedStateParent/>
+    </>
+  );
+}
+
+export default App
