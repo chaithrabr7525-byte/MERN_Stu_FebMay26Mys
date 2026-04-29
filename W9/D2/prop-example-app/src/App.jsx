0f16c2ab90bd54fd88bf94dd08d57aec8494938b
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-04-29 12:43:29 +0530

    passing object

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -5,6 +5,7 @@ import { PropsChildren } from './components/P3';
 import {PropDrillingDemo} from './components/P4.jsx'
 import{SharedStateParent} from './components/p5.jsx'
 import{CompositionDemo} from './components/p6.jsx'
+import{ObjectsAndArrays} from './components/p7.jsx'
 function App() {
   return (
     <>
@@ -13,7 +14,8 @@ function App() {
       {/* <PropsChildren/> */}
       {/* <PropDrillingDemo/> */}
       {/* <SharedStateParent/> */}
-      <CompositionDemo/>
+      {/* <CompositionDemo/> */}
+      <ObjectsAndArrays/>
     </>
   );
 }
