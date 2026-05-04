b8f03329ca6a6b5d87fef464da97a6aa21462422
chaithrabr7525-byte <chaithrabr7525@gmail.com>
2026-05-04 23:01:59 +0530

    app.js

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -7,7 +7,6 @@ app.use(express.json());
 app.use("/api/auth",authRoutes)
 app.use("/api/movies",movieRoutes);
 app.use("/api/booking",bookingRoutes);
-
 //base urls
 app.get("/", (req, res) => {
     res.status(200).json({
